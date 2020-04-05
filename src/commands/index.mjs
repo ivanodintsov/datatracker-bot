b31e53696a8b1a3d19dab3964582e003d020ce46
Ivan Odintsov <root@vm303632.had.su>
2020-04-05 03:28:58 +0300

    prod commands added

--- src/commands/index.mjs
+++ src/commands/index.mjs
@@ -1,4 +1,6 @@
 export { getStatsLink } from './getStatsLink';
 export { getMemberStatistics }  from './getMemberStatistics';
 export { getChatStatistics }  from './getChatStatistics';
-export { getMemberLink as get_member_stats }  from './getMemberLink';
+export { getMemberLink as member_link }  from './getMemberLink';
+export { getMemberStatistics as stat }  from './getMemberStatistics';
+export { getChatStatistics as chat_stat }  from './getChatStatistics';
