d7ceb2830468d1ce810c20d4eda51e1864524113
Ivan Odyntsov <iwan.odintsov@gmail.com>
2020-04-08 05:53:28 +0300

    default options for message job added

--- src/jobs/messagesQueue.mjs
+++ src/jobs/messagesQueue.mjs
@@ -1,6 +1,11 @@
 import Queue from 'bull';
 import { queues } from '../config'
 
-const messageQueue = new Queue('messages', queues);
+const messageQueue = new Queue('messages', {
+  ...queues,
+  defaultJobOptions: {
+    removeOnComplete: true,
+  },
+});
 
 export default messageQueue;
