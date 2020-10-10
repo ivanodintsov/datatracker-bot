9b2a0d7de26296b67e32f50d20219ee696ed1119
Ivan Odintsov <iwan.odintsov@gmail.com>
2020-10-11 00:28:24 +0300

    empty config added

---
+++ src/config.mjs
@@ -0,0 +1,29 @@
+export const botConfig = {
+  token: '',
+  parseMode: 'Markdown'
+};
+
+export const username = 'USERNAME';
+export const siteUrl = 'SITE_URL';
+
+export const bugsnagConfig = {
+  token: 'TOKEN'
+};
+
+export const redis = {
+  port: 6379,
+  host: '127.0.0.1',
+  password: '',
+};
+
+export const queues = {
+  redis,
+};
+
+export const graphql = {
+  url: 'URL',
+  method: 'METHOD',
+  token: 'TOKEN',
+};
+
+export const statsSiteUrl = 'URL';
